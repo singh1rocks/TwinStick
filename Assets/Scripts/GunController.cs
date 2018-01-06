755e19421df92a91c42d35dfb39fff2c93191c6a
singh1rocks <shakti.das@aol.com>
2018-01-05 21:33:42 -0700

    kamehameHAAAAAAAAAA

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public bool isFiring;
+
+    public BulletController bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform firePoint;
+    private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFiring && Time.time > nextFire)
+        {
+            nextFire = Time.time + timeBetweenShots;
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+    public GunController theGun;
+    public GameObject Juggernaut;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength)) {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            theGun.isFiring = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            theGun.isFiring = false;
+        }
+	}
+
+    void FixedUpdate () {
+        myRigidbody.velocity = moveVelocity;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Weapon 1")) {
+            other.gameObject.SetActive(false);
+            Destroy(gameObject);
+            Instantiate(Juggernaut, transform.position, transform.rotation);
+        } else if (other.gameObject.CompareTag("Weapon 2"))
+        {
+            other.gameObject.SetActive(false);
+        } else if (other.gameObject.CompareTag("Weapon 3"))
+        {
+            other.gameObject.SetActive(false);
+        } else if (other.gameObject.CompareTag("Weapon 4"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
